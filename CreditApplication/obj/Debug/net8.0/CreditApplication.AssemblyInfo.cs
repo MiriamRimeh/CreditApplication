ff0a8148236cb28f6bc485fd8dfa75b2d8d76b9d
mrimeh <34709087+MiriamRimeh@users.noreply.github.com>
2025-05-24 22:12:38 +0300

    Added a picture, new font and some color
    The custom colors don't work. Should resolve to bootstrap theming.

--- CreditApplication/obj/Debug/net8.0/CreditApplication.AssemblyInfo.cs
+++ CreditApplication/obj/Debug/net8.0/CreditApplication.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CreditApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+442383290615b417cd8f82ecfbefbaa1a3e7e8af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+251cff974572450408804fdd742200eae3037a38")]
 [assembly: System.Reflection.AssemblyProductAttribute("CreditApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CreditApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
