e135cc968b0b34be64b807d10d47b1c03e612cb2
mrimeh <34709087+MiriamRimeh@users.noreply.github.com>
2025-05-25 11:16:44 +0300

    Fixed stuff

--- CreditApplication/obj/Debug/net8.0/CreditApplication.AssemblyInfo.cs
+++ CreditApplication/obj/Debug/net8.0/CreditApplication.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CreditApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b41903912291fb2f5d0a44ceed0ba6bd4589e053")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+542fd530f762cec6ecb09373d8a23c3f13cbc362")]
 [assembly: System.Reflection.AssemblyProductAttribute("CreditApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CreditApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
